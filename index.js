873658a9774b30fda483db768e01bf0f689a8e87
snowcode132 <164957185+snowcode132@users.noreply.github.com>
2025-03-27 22:03:45 -0500

    added loading heart animation

---
+++ index.js
@@ -0,0 +1,4 @@
+function startHeart() {
+    document.querySelector("button").style.display = "none"; // Hide button
+    document.querySelector(".heart-container").style.display = "flex"; // Show heart & text container
+}
